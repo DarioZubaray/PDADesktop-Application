a879a63a634a3537be0b1bfd35eb3bc2fbd9a2ab
Dario Jose Zubaray <dzubaray@gmail.com>
2018-08-31 11:48:01 -0300

    agregando la clase CookieManager

--- Navegacion/App.xaml.cs
+++ Navegacion/App.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using log4net;
+using Navegacion.Classes;
 using Navegacion.View;
 using System;
 using System.Configuration;
-using System.Globalization;
 using System.Windows;
 
 namespace Navegacion
@@ -30,11 +30,15 @@ namespace Navegacion
         {
             base.OnStartup(e);
             logger.Info("Verificando datos guardados...");
-            VerificarDatosGuardados();
+            string usuario = VerificarDatosGuardados();
+            if(usuario != null)
+            {
+                logger.Info("cookie de usuario encontrada: " + usuario);
+            }
+
             Random rnd = new Random();
             // Genera un numero mayor a 0 y menor a 2 
             int numRandom = rnd.Next(0, 2);
-
 
             logger.Debug("Numero aleatorio generado: " + numRandom);
             if(numRandom == 1)
@@ -54,24 +58,9 @@ namespace Navegacion
             }
         }
 
-        private void VerificarDatosGuardados()
+        private string VerificarDatosGuardados()
         {
-            string urlFromProperties = ConfigurationManager.AppSettings.Get("URL_COOKIE");
-            string urlCookie = Environment.ExpandEnvironmentVariables(urlFromProperties);
-            Uri uriCookie = new Uri(urlCookie);
-            logger.Info(urlCookie);
-            try
-            {
-                string cookie = Application.GetCookie(uriCookie);
-
-                logger.Info("Cookie encontrada: " + cookie);
-                string nombre = cookie.Substring(cookie.IndexOf("=")+1);
-                logger.Info("nombre obtenido: " + nombre);
-            }
-            catch(Exception e)
-            {
-                logger.Error(e.GetBaseException().ToString());
-            }
+            return CookieManager.getCookie(CookieManager.Cookie.usuario);
         }
 
     }
---
+++ Navegacion/Classes/CookieManager.cs
@@ -0,0 +1,90 @@
+﻿using log4net;
+using System;
+using System.Configuration;
+using System.Windows;
+
+namespace Navegacion.Classes
+{
+    class CookieManager
+    {
+        #region atributes
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private string username;
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public enum Cookie { usuario, contraseña, ultimoAcceso };
+        #endregion
+
+        #region Methods
+        public static string getCookie(Cookie galletita)
+        {
+            string cookie2Return = null;
+
+            Uri uriCookie = getUriCookie(galletita);
+            try
+            {
+                cookie2Return = Application.GetCookie(uriCookie);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.GetBaseException().ToString());
+            }
+            return parseCookie2String(cookie2Return);
+        }
+
+        public static void setCookie(Cookie galletita, string valor)
+        {
+            Uri uriCookie = getUriCookie(galletita);
+            Application.SetCookie(uriCookie, valor);
+        }
+
+        private static Uri getUriCookie(Cookie galletita)
+        {
+            string baseUrlProperties = ConfigurationManager.AppSettings.Get("URL_COOKIE");
+            string baseUrlExpanded = Environment.ExpandEnvironmentVariables(baseUrlProperties);
+            string completeUrl = null;
+            switch (galletita)
+            {
+                case Cookie.usuario:
+                    completeUrl = baseUrlExpanded + "usuario";
+                    break;
+                case Cookie.contraseña:
+                    completeUrl = baseUrlExpanded + "contraseña";
+                    break;
+                case Cookie.ultimoAcceso:
+                    completeUrl = baseUrlExpanded + "ultimoAcceso";
+                    break;
+                default:
+                    return null;
+            }
+            logger.Info("url cookie: " + completeUrl);
+            return new Uri(completeUrl);
+        }
+
+        private static string parseCookie2String(string galletita)
+        {
+            if (galletita != null)
+            {
+                string llave = galletita.Substring(0, galletita.IndexOf("="));
+                string valor = galletita.Substring(galletita.IndexOf("=") + 1);
+                logger.Info("nombre obtenido: " + llave + " -> " + valor);
+                return valor;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+    }
+}
