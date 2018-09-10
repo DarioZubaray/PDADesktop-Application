568785f42cde42bfea4490d5b080ae3f3cfc2617
Dario Jose Zubaray <dzubaray@gmail.com>
2018-09-10 14:49:35 -0300

    añadiendo una clase estatica de propiedades globales

---
+++ PDADesktop/Classes/MyAppProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDADesktop.Classes
+{
+    class MyAppProperties
+    {
+        public static string idSucursal { get; set; }
+        public static string idLoteActual { get; set; }
+    }
+}
