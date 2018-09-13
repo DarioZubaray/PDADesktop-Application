add1bc99ee0a84e7bfa78da111d2f3514d36f58e
Dario Jose Zubaray <dzubaray@gmail.com>
2018-09-13 16:28:20 -0300

    agregando las clases de utlidades FileUtils para acarrear con logica de archivo y TextUtils para manipular texto

---
+++ PDADesktop/Utils/Fileutils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDADesktop.Utils
+{
+    class FileUtils
+    {
+        public static string ReadFile(string source)
+        {
+            string text = File.ReadAllText(@"c:\file.txt", Encoding.UTF8);
+            return text;
+        }
+    }
+}
---
+++ PDADesktop/Utils/TextUtils.cs
@@ -0,0 +1,51 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDADesktop.Utils
+{
+    class TextUtils
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static string ParseAjusteDAT2Json(string source)
+        {
+            StringBuilder ajusteJSON = new StringBuilder();
+            ajusteJSON.Append("[");
+            String[] lineas = source.Split( new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            int enBaseCero = -1;
+            int tamanioArray = lineas.Length + enBaseCero;
+            for (int i = 0; i <= tamanioArray; i++)
+            {
+                String ajuste = lineas[i];
+                if (ajuste.Contains("|"))
+                {
+                    String[] item = ajuste.Split('|');
+                    ajusteJSON.Append("{\"ean\": " + item[0]);
+                    ajusteJSON.Append(", \"fechaAjuste\": " + item[1]);
+                    ajusteJSON.Append(", \"motivo\": \"" + item[2] + "\"");
+                    ajusteJSON.Append(", \"perfilGenesix\": \"" + item[3] + "\"");
+                    ajusteJSON.Append(", \"cantidad\": " + item[4]);
+                    ajusteJSON.Append(", \"claveAjuste\": \"" + item[5] + "\"}");
+                    if (i < tamanioArray)
+                    {
+                        ajusteJSON.Append(",");
+                    }
+                    else
+                    {
+                        ajusteJSON.Append("]");
+                    }
+                }
+                else
+                {
+                    logger.Info("VA - parseAjusteDAT2Json linea skipeada: " + ajuste);
+                }
+            }
+            return ajusteJSON.ToString();
+
+        }
+    }
+}
