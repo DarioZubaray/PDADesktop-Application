f5a75a537fb3fecf3a15f81ccc37ce1f759328ad
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-19 16:52:03 -0300

    creando la grilla en imprimir recepciones, bindeando una lista de Recepcion

--- PDADesktop/ViewModel/ImprimirRecepcionViewModel.cs
+++ PDADesktop/ViewModel/ImprimirRecepcionViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using log4net;
+using PDADesktop.Model;
+using System.Collections.ObjectModel;
 
 namespace PDADesktop.ViewModel
 {
@@ -6,5 +8,18 @@ namespace PDADesktop.ViewModel
     {
         private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        private ObservableCollection<Recepcion> receptions;
+        public ObservableCollection<Recepcion> Receptions
+        {
+            get
+            {
+                return receptions;
+            }
+            set
+            {
+                receptions = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
