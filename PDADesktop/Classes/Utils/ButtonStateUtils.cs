df785faf2d9df92699934f0255701cc492b23c9b
Dario Jose Zubaray <dzubaray@gmail.com>
2018-10-24 12:31:44 -0300

    rename variables to english

--- PDADesktop/Classes/Utils/ButtonStateUtils.cs
+++ PDADesktop/Classes/Utils/ButtonStateUtils.cs
@@ -18,36 +18,36 @@ namespace PDADesktop.Classes.Utils
         {
             bool stateNeedResolve = true;
             SincronizacionDtoDataGrid dto = MyAppProperties.SelectedSync;
-            int estadoGeneral = dto.idEstadoGeneral;
-            long idSincronizacion = dto.idSincronizacion;
-            string idLote = dto.lote;
-            int idActividad = dto.idActividad;
-            int idAccion = dto.idAccion;
-            int egx = dto.idEstadoGenesix;
-            int epda = dto.idEstadoPda;
+            int generalState = dto.idEstadoGeneral;
+            long synchronizationId = dto.idSincronizacion;
+            string batchId = dto.lote;
+            int activityId = dto.idActividad;
+            int actionId = dto.idAccion;
+            int genesixState = dto.idEstadoGenesix;
+            int deviceState = dto.idEstadoPda;
 
-            switch(estadoGeneral)
+            switch(generalState)
             {
                 case Constants.EGRAL_REINTENTAR_INFORMAR:
-                    RetryInformToGenesix(idSincronizacion, idActividad);
+                    RetryInformToGenesix(synchronizationId, activityId);
                     break;
                 case Constants.EGRAL_REINTENTAR_DESCARGA:
-                    RetryDownloadFromGenesix(idSincronizacion, idAccion, idActividad);
+                    RetryDownloadFromGenesix(synchronizationId, actionId, activityId);
                     break;
                 case Constants.EGRAL_REINTENTAR3:
-                    RetryDownloadReceptionsFromGenesix(idSincronizacion, idLote);
+                    RetryDownloadReceptionsFromGenesix(synchronizationId, batchId);
                     break;
                 case Constants.EGRAL_MODIFICAR_AJUSTE:
-                    ModifyAdjusments(idSincronizacion, idLote);
+                    ModifyAdjusments(synchronizationId, batchId);
                     break;
                 case Constants.EGRAL_VER_DETALLES:
-                    seeReceptionsDetails(idSincronizacion, idLote);
+                    seeReceptionsDetails(synchronizationId, batchId);
                     break;
                 case Constants.EGRAL_IMPRIMIR_RECEPCION:
-                    PrintReceptions(idSincronizacion, idLote);
+                    PrintReceptions(synchronizationId, batchId);
                     break;
                 case Constants.EGRAL_OK:
-                    SeeAdjustmentsInforms(idActividad, epda, egx, idLote);
+                    SeeAdjustmentsInforms(activityId, deviceState, genesixState, batchId);
                     break;
                 default:
                     stateNeedResolve = false;
@@ -56,52 +56,70 @@ namespace PDADesktop.Classes.Utils
             return stateNeedResolve;
         }
 
-        private static void SeeAdjustmentsInforms(int idActividad, int epda, int egx, string idLote)
+        private static void SeeAdjustmentsInforms(int activityId, int deviceState, int genesixState, string batchId)
         {
-            if ( Constants.EPDA_RECIBIDO.Equals(epda)
-                 && Constants.EGX_ENVIADO.Equals(egx)
-                 && Constants.ACTIVIDAD_AJUSTES.Equals(idActividad) )
+            if ( Constants.EPDA_RECIBIDO.Equals(deviceState)
+                 && Constants.EGX_ENVIADO.Equals(genesixState)
+                 && Constants.ACTIVIDAD_AJUSTES.Equals(activityId) )
             {
                 MainWindow window = (MainWindow)Application.Current.MainWindow;
-                Uri uri = new Uri(Constants.VER_AJUSTES_INFORMADOS_VIEW, UriKind.Relative);
-                window.frame.NavigationService.Navigate(uri);
+                Uri uriSeeAdjustmentsInform = new Uri(Constants.VER_AJUSTES_INFORMADOS_VIEW, UriKind.Relative);
+                window.frame.NavigationService.Navigate(uriSeeAdjustmentsInform);
             }
         }
 
-        private static void PrintReceptions(long idSincronizacion, string idLote)
+        private static void PrintReceptions(long syncId, string batchId)
         {
+            // Cambia estado de sincro a OK y refresca
+            SynchronizationStateUtil.SetSentGenesixState(syncId);
             //lamar a la vista 'ImprimirRecepcionView'
             MainWindow window = (MainWindow)Application.Current.MainWindow;
-            Uri uri = new Uri(Constants.IMPRIMIR_RECEPCION_VIEW, UriKind.Relative);
-            window.frame.NavigationService.Navigate(uri);
-            //la cual puede abrir un PDF
+            Uri uriPrintReceptions = new Uri(Constants.IMPRIMIR_RECEPCION_VIEW, UriKind.Relative);
+            window.frame.NavigationService.Navigate(uriPrintReceptions);
         }
 
-        private static void seeReceptionsDetails(long idSincronizacion, string idLote)
+        private static void seeReceptionsDetails(long syncId, string batchId)
         {
             //lamar a la vista 'VerDetallesRecepcionView'
             MainWindow window = (MainWindow)Application.Current.MainWindow;
-            Uri uri = new Uri(Constants.VER_DETALLES_RECEPCION_VIEW, UriKind.Relative);
-            window.frame.NavigationService.Navigate(uri);
+            Uri uriSeeReceptionsDetails = new Uri(Constants.VER_DETALLES_RECEPCION_VIEW, UriKind.Relative);
+            window.frame.NavigationService.Navigate(uriSeeReceptionsDetails);
         }
 
-        private static void ModifyAdjusments(long idSincronizacion, string idLote)
+        private static void ModifyAdjusments(long syncId, string batchId)
         {
             MainWindow window = (MainWindow)Application.Current.MainWindow;
-            Uri uri = new Uri(Constants.VER_AJUSTES_MODIFICAR_VIEW, UriKind.Relative);
-            window.frame.NavigationService.Navigate(uri);
+            Uri uriSeeAdjustmentsModify = new Uri(Constants.VER_AJUSTES_MODIFICAR_VIEW, UriKind.Relative);
+            window.frame.NavigationService.Navigate(uriSeeAdjustmentsModify);
         }
 
-        private static void RetryDownloadReceptionsFromGenesix(long idSincronizacion, string idLote)
+        private static void RetryDownloadReceptionsFromGenesix(long syncId, string batchId)
         {
             IDeviceHandler deviceHandler = App.Instance.deviceHandler;
             bool doControls = ControlStoreIdAndDatetimeSync();
             if (doControls)
             {
                 string storeId = MyAppProperties.storeId;
-                DownloadFromGenesix(Constants.ACTIVIDAD_INFORMAR_RECEPCIONES, storeId, idSincronizacion);
+                DownloadFromGenesix(Constants.ACTIVIDAD_INFORMAR_RECEPCIONES, storeId, syncId);
 
-                ControlBloqueoPDA desbloquearPDA = deviceHandler.ControlDeviceLock(idSincronizacion, storeId);
+                ControlBloqueoPDA unlockDevice = deviceHandler.ControlDeviceLock(syncId, storeId);
+                if (unlockDevice.desbloquearPDA)
+                {
+                    deviceHandler.ChangeSynchronizationState(Constants.ESTADO_SINCRO_FIN);
+                }
+            }
+        }
+
+        private static void RetryDownloadFromGenesix(long syncId, int actionId, int activityId)
+        {
+            IDeviceHandler deviceHandler = App.Instance.deviceHandler;
+            bool doControls = ControlStoreIdAndDatetimeSync();
+            if (doControls)
+            {
+                string storeId = MyAppProperties.storeId;
+                DownloadFromGenesix(activityId, storeId, syncId);
+
+                ControlBloqueoPDA desbloquearPDA = deviceHandler.ControlDeviceLock(syncId, storeId);
                 if (desbloquearPDA.desbloquearPDA)
                 {
                     deviceHandler.ChangeSynchronizationState(Constants.ESTADO_SINCRO_FIN);
@@ -109,27 +127,10 @@ namespace PDADesktop.Classes.Utils
             }
         }
 
-        private static void RetryDownloadFromGenesix(long idSincronizacion, int idAccion, int idActividad)
+        private static void RetryInformToGenesix(long syncId, int activityId)
         {
             IDeviceHandler deviceHandler = App.Instance.deviceHandler;
-            bool doControls = ControlStoreIdAndDatetimeSync();
-            if (doControls)
-            {
-                string storeId = MyAppProperties.storeId;
-                DownloadFromGenesix(idActividad, storeId, idSincronizacion);
-
-                ControlBloqueoPDA desbloquearPDA = deviceHandler.ControlDeviceLock(idSincronizacion, storeId);
-                if (desbloquearPDA.desbloquearPDA)
-                {
-                    deviceHandler.ChangeSynchronizationState(Constants.ESTADO_SINCRO_FIN);
-                }
-            }
-        }
-
-        private static void RetryInformToGenesix(long idSincronizacion, int idActividad)
-        {
-            IDeviceHandler deviceHandler = App.Instance.deviceHandler;
-            if (Constants.ACTIVIDAD_INFORMAR_RECEPCIONES.Equals(idActividad))
+            if (Constants.ACTIVIDAD_INFORMAR_RECEPCIONES.Equals(activityId))
             {
                 bool doControls = ControlStoreIdAndDatetimeSync();
                 if (doControls)
@@ -138,9 +139,9 @@ namespace PDADesktop.Classes.Utils
                 }
             }
 
-            HttpWebClientUtil.ExecuteInformGenesix(idSincronizacion);
+            HttpWebClientUtil.ExecuteInformGenesix(syncId);
             string storeId = MyAppProperties.storeId;
-            ControlBloqueoPDA unlockDevice = deviceHandler.ControlDeviceLock(idSincronizacion, storeId);
+            ControlBloqueoPDA unlockDevice = deviceHandler.ControlDeviceLock(syncId, storeId);
             if (unlockDevice.desbloquearPDA)
             {
                 deviceHandler.ChangeSynchronizationState(Constants.ESTADO_SINCRO_FIN);
@@ -187,25 +188,25 @@ namespace PDADesktop.Classes.Utils
             }
         }
 
-        public static void DownloadFromGenesix(int idActividad, string storeId, long syncId)
+        public static void DownloadFromGenesix(int activityId, string storeId, long syncId)
         {
-            bool descargaMaestroCorrecta = HttpWebClientUtil.SearchDATsMasterFile(idActividad, storeId);
+            bool downloadSuccess = HttpWebClientUtil.SearchDATsMasterFile(activityId, storeId);
 
-            if (descargaMaestroCorrecta)
+            if (downloadSuccess)
             {
-                if (idActividad.Equals(Constants.UBICART_CODE))
+                if (activityId.Equals(Constants.UBICART_CODE))
                 {
                     logger.Debug("Ubicart -> creando Archivos PAS");
                     ArchivosDATUtils.createPASFile();
                 }
-                if (idActividad.Equals(Constants.PEDIDOS_CODE))
+                if (activityId.Equals(Constants.PEDIDOS_CODE))
                 {
                     logger.Debug("Pedidos -> creando Archivos Pedidos");
                     ArchivosDATUtils.createOrdersFiles();
                 }
 
                 string destinationDirectory = ConfigurationManager.AppSettings.Get(Constants.DEVICE_RELPATH_DATA);
-                string filename = ArchivosDATUtils.GetDataFileNameByIdActividad(idActividad);
+                string filename = ArchivosDATUtils.GetDataFileNameByIdActividad(activityId);
                 string filenameAndExtension = FileUtils.WrapSlashAndDATExtension(filename);
 
                 ResultFileOperation result = App.Instance.deviceHandler.CopyPublicDataFileToDevice(destinationDirectory, filenameAndExtension);
