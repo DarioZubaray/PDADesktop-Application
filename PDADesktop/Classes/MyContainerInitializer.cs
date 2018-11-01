4b481e3592574d6e50768d12e8ba62ad6332939b
Dario Jose Zubaray <dzubaray@gmail.com>
2018-11-01 11:12:35 -0300

    declarando los tipos concretos de los modelview para la aplicacion en mi Container Initializer

--- PDADesktop/Classes/MyContainerInitializer.cs
+++ PDADesktop/Classes/MyContainerInitializer.cs
@@ -18,10 +18,16 @@ namespace PDADesktop.Classes
 
             ForConcreteType<MainWindowViewModel>().Configure.Singleton();
 
-            ForConcreteType<CentroActividadesViewModel>().Configure.Singleton();
             ForConcreteType<LoginViewModel>().Configure.Singleton();
+
+            ForConcreteType<CentroActividadesViewModel>().Configure.Singleton();
+
             ForConcreteType<VerAjustesRealizadosViewModel>().Configure.Singleton();
+            ForConcreteType<VerAjustesModificarViewModel>().Configure.Singleton();
             ForConcreteType<VerAjustesInformadosViewModel>().Configure.Singleton();
+
+            ForConcreteType<VerDetallesRecepcionViewModel>().Configure.Singleton();
+            ForConcreteType<ImprimirRecepcionViewModel>().Configure.Singleton();
 
             string deviceHandler = ConfigurationManager.AppSettings.Get(Constants.DEVICE_HANDLER);
             if (deviceHandler.Equals(Constants.DESKTOP_ADAPTER, StringComparison.InvariantCultureIgnoreCase))
