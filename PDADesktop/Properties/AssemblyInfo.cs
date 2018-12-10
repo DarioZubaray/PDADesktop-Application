837e3bf06075f713ced932444019212b11f50b14
Dario Zubaray <dzubaray@gmail.com>
2018-12-10 15:52:38 -0300

    incrementando la version del assembly a 1.0.6

--- PDADesktop/Properties/AssemblyInfo.cs
+++ PDADesktop/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
