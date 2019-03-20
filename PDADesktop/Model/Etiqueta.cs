dbe6a1737b990ff86cf71f178b4f9138f7d18cb4
Dario Zubaray <dzubaray@gmail.com>
2019-03-20 10:13:55 -0300

    se modifica los artributos de fechas del modelo de etiquetas

--- PDADesktop/Model/Etiqueta.cs
+++ PDADesktop/Model/Etiqueta.cs
@@ -1,43 +1,30 @@
-﻿using System;
+﻿using PDADesktop.Classes.Utils;
+using System;
 using System.Globalization;
 
 namespace PDADesktop.Model
 {
     public class Etiqueta
     {
-        private string _fecha;
-        private DateTime _fechaDate;
 
         public static readonly string DATE_FORMAT = "yyyyMMddHHmmss";
 
         public long ID { get; set; }
         public string EAN { get; set; }
         public string CodigoEtiqueta { get; set; }
+
         public string Fecha
         {
             get
             {
-                return _fecha;
+                return DateTimeUtils.GetActivityStringDate(FechaDate);
             }
             set
             {
-                //_fechaDate = DateTime.ParseExact(value, DATE_FORMAT, CultureInfo.InvariantCulture);
-                _fecha = value;
+                FechaDate = DateTimeUtils.GetActivityDatetime(value);
             }
         }
 
-        public DateTime FechaDate
-        {
-            get
-            {
-                return _fechaDate;
-            }
-            set
-            {
-                _fechaDate = value;
-                var dateString = value.ToString(DATE_FORMAT, CultureInfo.InvariantCulture);
-                _fecha = dateString;
-            }
-        }
+        public DateTime FechaDate { get; set; }
     }
 }
